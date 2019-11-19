7ea117e5b5204fa9df1448f685e1fd19612b6f1e
taylor-meyer <taylor.ross.meyer@gmail.com>
2019-11-19 11:33:13 -0800

    Splits music.json into three files dynamically

---
+++ src/filesplit/Main.java
@@ -0,0 +1,18 @@
+
+package filesplit;
+
+/**
+ *
+ * @author Taylor Meyer
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Splitter s = new Splitter();
+    }
+    
+}
