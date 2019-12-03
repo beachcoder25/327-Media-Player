bf3bb255a3360bd9e869a86bf5cf5897356bda8f
nickamcd <37389764+nickamcd@users.noreply.github.com>
2019-12-02 19:08:06 -0800

    Comments
    Added in comments for logic, not sure if it is totally correct implementation yet.

--- src/Phase4_TwoPhaseCommit/Cohort.java
+++ src/Phase4_TwoPhaseCommit/Cohort.java
@@ -13,11 +13,25 @@ public class Cohort implements CohortInterface {
 
     @Override
     public void haveCommitted(Transaction transaction) {
+        // before commit, save files to temp folder (.../guid/temp)
+            // build out pathfile like in chord?
+        
+        // let coordinator know that you have committed
+        
+        // sleep for allotted time
+        // if no response, then call get decision
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean getDecision(Transaction transaction) {
+        // check in with server to see if all participants voted yes and have committed
+        // coordinator sends back yes or no if can still commit
+            // coordinator checks for all responses
+            // if still missing responses, coord sends abort to all participants
+        
+        // to rollback, grab files from temp
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
--- src/Phase4_TwoPhaseCommit/Coordinator.java
+++ src/Phase4_TwoPhaseCommit/Coordinator.java
@@ -5,8 +5,6 @@
  */
 package Phase4_TwoPhaseCommit;
 
-import Phase3_Other.ChordMessageInterface;
-
 /**
  *
  * @author nicka
@@ -15,19 +13,35 @@ public class Coordinator implements CoordinatorInterface {
 
     @Override
     public boolean canCommit(Transaction transaction) {
+        try {
+            // create group of peers (cohorts) to send signal to
+            
+            // send transaction to get vote?
+            
+            // sleep to wait for vote
+                // if sloloted time passes and not all participants have responded
+                // send out doAbort and break
+            
+            // otherwise send out doCommit
+        } catch (Exception e) {
+            
+        }
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void doCommit(Transaction transaction) {
+        // tell coord to commit
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void doAbort(Transaction transaction) {
+        // tell coord to abort transaction
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
-    
+    //
     
 }
